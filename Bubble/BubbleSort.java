b037989d4792065d144c109622cd055cc7a2ad43
Muhammad syahril <m.yafiheldy@gmail.com>
2023-12-26 12:54:59 +0700

    testing

---
+++ Bubble/BubbleSort.java
@@ -0,0 +1,40 @@
+package Bubble;
+
+public class BubbleSort {
+
+    
+    void bubbleSort(int arr[]) {
+        int n = arr.length; // Mendapatkan panjang dari array
+
+        // Loop luar dari awal sampai hampir akhir array
+        for (int i = 0; i < n-1; i++) {
+            
+            // Loop dalam untuk perbandingan dan pertukaran
+            for (int j = 0; j < n-i-1; j++) {
+                
+                // Jika elemen saat ini lebih besar dari yang berikutnya
+                if (arr[j] > arr[j+1]) {
+                    // Pertukaran elemen
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    
+    public static void main(String args[]) {
+        BubbleSort ob = new BubbleSort(); // Membuat instansi dari kelas BubbleSort
+        int arr[] = {64, 34, 25, 12, 22, 11, 90}; // Array yang akan diurutkan
+
+        ob.bubbleSort(arr); // Memanggil metode bubbleSort
+
+        System.out.println("Sorted array"); // Mencetak pesan
+
+        // Loop untuk mencetak array yang sudah diurutkan
+        for (int i=0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
